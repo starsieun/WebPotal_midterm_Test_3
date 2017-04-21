8f6cc4bd5a1268c347317dc517496ae6af8ce590
starsieun <risieun513@naver.com>
2017-04-21 09:51:18 +0900

    userdao_refactoring_1

--- userdao/src/kr/ac/jejunu/UserDao.java
+++ userdao/src/kr/ac/jejunu/UserDao.java
@@ -18,25 +18,12 @@ public class UserDao {
 
     public User get(String id) throws ClassNotFoundException, SQLException {
 
-        //Class 로딩해야하네
-        Class.forName("com.mysql.jdbc.Driver");
+        Connection connection = getConnection();
 
-        //커넥션맺기
-
-
-        Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.106/sieun1","root","1234");
-
-            //쿼리만들기
         PreparedStatement preparedStatement = connection.prepareStatement("select * from user where id = ?");
         preparedStatement.setString(1, id);
 
-
-
-
-            //쿼리실행
-
         ResultSet resultSet = preparedStatement.executeQuery();
-            //생성된결과를 객체 매핑
 
         resultSet.next();
         User user = new User();
@@ -44,24 +31,20 @@ public class UserDao {
         user.setName(resultSet.getString("name"));
         user.setPassword(resultSet.getString("password"));
 
-            //자원해지
 
         resultSet.close();
         preparedStatement.close();
         connection.close();
 
-            //결과를 리턴
-
         return user;
     }
 
+
+
     public void add(User user) throws ClassNotFoundException, SQLException {
 
-        //Class 로딩해야하네
-        Class.forName("com.mysql.jdbc.Driver");
 
-
-        Connection connection = DriverManager.getConnection("jdbc:mysql://117.17.102.106/sieun1","root","1234");
+        Connection connection = getConnection();
 
 
         PreparedStatement preparedStatement = connection.prepareStatement("insert into user(id, name, password) VALUE (?, ?, ?)");
@@ -71,21 +54,15 @@ public class UserDao {
 
 
 
-
-        //쿼리실행
-
         preparedStatement.executeUpdate();
-        //생성된결과를 객체 매핑
-
-        //자원해지
-
 
         preparedStatement.close();
         connection.close();
 
-        //결과를 리턴
+    }
 
-
-
+    private Connection getConnection() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.jdbc.Driver");
+        return DriverManager.getConnection("jdbc:mysql://117.17.102.106/sieun1","root","1234");
     }
 }
